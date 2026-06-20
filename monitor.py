237974e7d5fdbfcf809f8f8c9056a6a0cb122a31
Ivan10588 <Ivanov_Ivan_88@mail.ru>
2026-06-20 14:27:00 +0700

    Lesson_59

--- monitor.py
+++ monitor.py
@@ -2,3 +2,9 @@ import psutil
 import time
 import os
 from datetime import datetime
+
+def bytes_to_gb(bytes_value):
+    """Конвертирует байты в гигабайты с округлением до 2 знаков."""
+    return round(bytes_value / (1024 ** 3), 2)
+
+
