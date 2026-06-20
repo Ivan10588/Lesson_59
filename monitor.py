870b15295c48cc1db535952f91c6bd5ee5470ca6
Ivan10588 <Ivanov_Ivan_88@mail.ru>
2026-06-20 14:29:53 +0700

    Lesson_59

--- monitor.py
+++ monitor.py
@@ -7,4 +7,39 @@ def bytes_to_gb(bytes_value):
     """Конвертирует байты в гигабайты с округлением до 2 знаков."""
     return round(bytes_value / (1024 ** 3), 2)
 
+def get_cpu_info(use_interval_none=False):
+    """
+    Собирает информацию о загрузке CPU.
+    Если use_interval_none=True, использует интервал None (дельта с прошлого замера).
+    Иначе делает замер с интервалом 1 сек.
+    Возвращает словарь с текущей загрузкой и средними значениями (если доступны).
+    """
+    if use_interval_none:
+        current_percent = psutil.cpu_percent(interval=None)
+    else:
+        current_percent = psutil.cpu_percent(interval=1)
+
+    avg1, avg5, avg15 = "N/A", "N/A", "N/A"
+    try:
+        load_avg = psutil.getloadavg()
+        avg1, avg5, avg15 = [round(x, 1) for x in load_avg]
+    except AttributeError:
+        pass
+
+    return {
+        "current_percent": current_percent,
+        "avg1": avg1,
+        "avg5": avg5,
+        "avg15": avg15,
+    }
+
+def get_memory_info():
+    """Собирает информацию об использовании памяти (RAM)."""
+    mem = psutil.virtual_memory()
+    return {
+        "total_gb": bytes_to_gb(mem.total),
+        "available_gb": bytes_to_gb(mem.available),
+        "used_gb": bytes_to_gb(mem.used),
+        "percent_used": round(mem.percent, 1),
+    }
 
